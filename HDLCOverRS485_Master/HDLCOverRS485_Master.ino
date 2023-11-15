a379c79242e52e6888f3c63e2380d1d0cc1b9a27
Guilherme Lopes de Oliveira <83103714+Guilherme227@users.noreply.github.com>
2023-11-15 00:19:28 -0300

    Add files via upload

--- HDLCOverRS485_Master/HDLCOverRS485_Master.ino
+++ HDLCOverRS485_Master/HDLCOverRS485_Master.ino
@@ -8,38 +8,100 @@ Uso do protocolo de enlace HDLC sobre o protocolo físico RS485.
 Código do Arduino UNO (Mestre)
 */
 
-const unsigned long TIMEOUT_DURATION = 1500;
+const unsigned long TIMEOUT_DURATION = 10000;
 const byte ACK_BYTE = 0x06;
 const byte NAK_BYTE = 0x15;
+const byte FLAG = 0x7E;
 const uint16_t SLAVE_ID_1 = 1;
 const uint16_t SLAVE_ID_2 = 2;
 const uint16_t BROADCAST = 3;
-//uint16_t selectedSlaveID = 0;
-const uint16_t CONTROL = 0x07;
+const uint16_t CONTROL_1 = 1; // LED verde
+const uint16_t CONTROL_2 = 2; // LED amarelo
+const uint16_t CONTROL_3 = 3; // LED vermelho
+const uint16_t CONTROL_4 = 4; // Todos LEDs
+uint16_t selectedID;
+uint16_t selectedControl;
 const char* messageToSend = "Hello ESP32!";
-const byte FLAG = 0x7E;
+
 
 void setup() {
   Serial.begin(9600); // Começa a comuncação serial
-  //selectedSlaveID = 0; // Inicialmente nenhum escravo selecionado
 }
 
 void loop() {
-  //if (selectedSlaveID != 0){ // Verifica se o usuário deseja enviar a mensagem
-    //sendHDLCMessage(selectedSlaveID, messageToSend);
-    sendHDLCMessage(SLAVE_ID_2, CONTROL, messageToSend);
-    //selectedSlaveID = 0; 
+    //makeSelection();
+    //sendHDLCMessage(selectedID, selectedControl, messageToSend);
+    sendHDLCMessage(SLAVE_ID_1, CONTROL_4, messageToSend);
 
-  /*if (Serial.available() > 0) {
-    char userInput = Serial.read();
-    if (userInput == '1') {
-      selectedSlaveID = SLAVE_ID_1;
-    } else if (userInput == '2') {
-      selectedSlaveID = SLAVE_ID_2;
-    } else if (userInput == 'c' || userInput == 'C') {
-      selectedSlaveID = 0;
-    }
-  }*/
+    /*if(!waitForACK) {
+      sendHDLCMessage(BROADCAST, CONTOL_4, messageToSend);
+    }*/
+
+    delay(1000);
+}
+
+void makeSelection() {
+  // Limpa o buffer
+  while (Serial.available() > 0) {
+    Serial.read();
+  }
+
+  // Escolher a ID
+  Serial.println("Escolha o ID:");
+  Serial.println("1 - Escravo 1");
+  Serial.println("2 - Escravo 2");
+  Serial.println("3 - Broadcast");
+
+  while (!Serial.available()) {
+    // Aguarda interação do usuário
+  }
+
+  selectedID = Serial.parseInt();
+
+  // Limpa o buffer novamente
+  while (Serial.available() > 0) {
+    Serial.read();
+  }
+
+  Serial.print("ID escolhida: ");
+  Serial.println(selectedID);
+  delay(1000);
+
+  // Escolher o controle
+  Serial.println("Escolha o valor de controle:");
+  Serial.println("1 - Pisca LED Verde");
+  Serial.println("2 - Pisca LED Amarelo");
+  Serial.println("3 - Pisca LED Vermelho");
+  Serial.println("4 - Pisca todos os LEDs");
+  Serial.println("5 - Controle inválido para teste");
+
+  while (!Serial.available()) {
+    // Aguarda interação do usuário
+  }
+
+  selectedControl = Serial.parseInt();
+
+  // Limpa o buffer novamente
+  while (Serial.available() > 0) {
+    Serial.read();
+  }
+
+  Serial.print("Controle escolhido: ");
+  Serial.println(selectedControl);
+  delay(1000);
+
+  Serial.println("Confirma?");
+  Serial.println("1 - Confirmar");
+  Serial.println("2 - Cancelar");
+  
+  while (!Serial.available()) {
+    // Aguarda interação do usuário
+  }
+
+  if (Serial.parseInt() == 2) {
+    makeSelection();
+    return;
+  }
 }
 
 void sendHDLCMessage(uint16_t slaveID, uint16_t control, const char* message){
@@ -57,31 +119,25 @@ void sendHDLCMessage(uint16_t slaveID, uint16_t control, const char* message){
   Serial.write((byte)fcs);
   Serial.write(FLAG); // Flag de final de mensagem
 
-  Serial.print("FCS calculado: ");
+  Serial.print("\nFCS calculado: ");
   Serial.println(fcs);
-
-  if (waitForACK()) {
-    Serial.println("Mensagem enviada com sucesso!");
-  } else {
-    Serial.println("Falha ao enviar a mensagem!");
-  }
 }
 
-bool waitForACK() {
+void waitForACK() {
   unsigned long startTime = millis();
 
   while (millis() - startTime < TIMEOUT_DURATION) {
-    if (Serial.available() > 0) {
+    while (Serial.available() > 0) {
       char receivedChar = Serial.read();
 
-      if (receivedChar == ACK_BYTE) {
-        return true;
-      } else if (receivedChar == NAK_BYTE) {
-        return false;
+      if (receivedChar == ACK_BYTE) { // ACK
+        Serial.println("Mensagem enviada com sucesso!");
+      } else if (receivedChar == NAK_BYTE) { // NAK
+        Serial.println("NAK recebido!");
       }
     }
   }
-  return false;
+  Serial.println("Timeout ao esperar ACK/NAK!"); // Timeout
 }
 
 uint16_t calculateFCS(uint16_t address, uint16_t control, const char* message) {
