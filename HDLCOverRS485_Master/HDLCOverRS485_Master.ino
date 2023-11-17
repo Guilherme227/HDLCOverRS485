f2944cf52085a81c588961bb222578ae9ff2ff30
Guilherme Lopes de Oliveira <83103714+Guilherme227@users.noreply.github.com>
2023-11-17 20:12:32 -0300

    Add files via upload

--- HDLCOverRS485_Master/HDLCOverRS485_Master.ino
+++ HDLCOverRS485_Master/HDLCOverRS485_Master.ino
@@ -8,9 +8,9 @@ Uso do protocolo de enlace HDLC sobre o protocolo físico RS485.
 Código do Arduino UNO (Mestre)
 */
 
-const unsigned long TIMEOUT_DURATION = 10000;
-const byte ACK_BYTE = 0x06;
-const byte NAK_BYTE = 0x15;
+const unsigned long TIMEOUT_DURATION = 5000;
+const char ACK_BYTE = 6;
+const char NAK_BYTE = 7;
 const byte FLAG = 0x7E;
 const uint16_t SLAVE_ID_1 = 1;
 const uint16_t SLAVE_ID_2 = 2;
@@ -22,6 +22,8 @@ const uint16_t CONTROL_4 = 4; // Todos LEDs
 uint16_t selectedID;
 uint16_t selectedControl;
 const char* messageToSend = "Hello ESP32!";
+bool firstMessage = false;
+bool secondMessage = false;
 
 
 void setup() {
@@ -31,13 +33,51 @@ void setup() {
 void loop() {
     //makeSelection();
     //sendHDLCMessage(selectedID, selectedControl, messageToSend);
-    sendHDLCMessage(SLAVE_ID_1, CONTROL_4, messageToSend);
+    //sendHDLCMessage(SLAVE_ID_2, CONTROL_1, messageToSend);
+    if(!firstMessage){
+      sendHDLCMessage (BROADCAST, CONTROL_4, messageToSend);
+      firstMessage = true;
+    }
+    /*switch(waitForACK()) {
+      case 1:
+        Serial.println("Mensagem enviada com sucesso!");
+      break;
 
-    /*if(!waitForACK) {
-      sendHDLCMessage(BROADCAST, CONTOL_4, messageToSend);
+      case 2:
+        Serial.println("NAK recebido!"); // NAK recebido
+        Serial.println("Enviando novamente:");
+        sendHDLCMessage(BROADCAST, CONTROL_4, messageToSend);
+      break;
+
+      case 3:
+        Serial.println("Timeout ao esperar ACK/NAK");
+        Serial.println("Enviando novamente:");
+        sendHDLCMessage(BROADCAST, CONTROL_4, messageToSend);
+      break;
+
+      default:
+        Serial.println("Algo deu errado!");
+      break;
     }*/
-
-    delay(1000);
+    if(waitForACK() == 1) { // ACK recebido
+      Serial.println("Mensagem enviada com sucesso!");
+      secondMessage = true;
+    }
+    if(waitForACK() == 2) { // NAK recebido
+      if(!secondMessage) {
+        Serial.println("NAK recebido!"); // NAK recebido
+        Serial.println("Enviando novamente:");
+        sendHDLCMessage(BROADCAST, CONTROL_4, messageToSend);
+      }
+    }
+    if(waitForACK() == 3) { // Timeout
+      if(!secondMessage) {
+        Serial.println("Timeout ao esperar ACK/NAK");
+        Serial.println("Enviando novamente:");
+        sendHDLCMessage(BROADCAST, CONTROL_4, messageToSend);  
+      }
+    }
+    delay(100);
 }
 
 void makeSelection() {
@@ -113,40 +153,59 @@ void sendHDLCMessage(uint16_t slaveID, uint16_t control, const char* message){
   Serial.write(message); // Mensagem
   Serial.flush(); // Espera toda mensagem ser enviada
 
-  uint16_t fcs = calculateFCS(slaveID, control, message); // Calcula o FCS
+  uint16_t crc = calculateCRC(slaveID, control, message); // Calcula o FCS
 
-  Serial.write((byte)(fcs >> 8)); // FCS
-  Serial.write((byte)fcs);
+  Serial.write((byte)(crc >> 8)); // FCS
+  Serial.write((byte)crc);
   Serial.write(FLAG); // Flag de final de mensagem
 
-  Serial.print("\nFCS calculado: ");
-  Serial.println(fcs);
+  Serial.print("\nCRC calculado: ");
+  Serial.println(crc);
 }
 
-void waitForACK() {
+int waitForACK() {
   unsigned long startTime = millis();
+  bool receivedResponse = false;
 
-  while (millis() - startTime < TIMEOUT_DURATION) {
+  while (millis() - startTime < TIMEOUT_DURATION) { // Comparação para saber se deu o tempo de timeout
     while (Serial.available() > 0) {
       char receivedChar = Serial.read();
 
+      //Serial.print("Recebido: ");
+      //Serial.println(receivedChar);
+
       if (receivedChar == ACK_BYTE) { // ACK
-        Serial.println("Mensagem enviada com sucesso!");
+        if(!receivedResponse) {
+          //Serial.println("Recebi ACK");
+          //Serial.println("Mensagem enviada com sucesso!");
+          receivedResponse = true;
+          return 1;
+        }
       } else if (receivedChar == NAK_BYTE) { // NAK
-        Serial.println("NAK recebido!");
-      }
+          if(!receivedResponse) {
+            //Serial.println("Recebi NAK");
+            //Serial.println("NAK recebido!");
+            receivedResponse = true;
+            return 2;
+          }
+        }
     }
   }
-  Serial.println("Timeout ao esperar ACK/NAK!"); // Timeout
+  if (millis() - startTime >= TIMEOUT_DURATION) { // Timeout
+    if(!receivedResponse) {
+      //Serial.println("Timeout ao esperar ACK/NAK!");
+      return 3;
+    }
+  }
 }
 
-uint16_t calculateFCS(uint16_t address, uint16_t control, const char* message) {
-  uint16_t fcs = 0;
-  fcs ^= address; // XOR do valor atual do fcs com address
-  fcs ^= control; // XOR do valor atual do fcs com control
+uint16_t calculateCRC(uint16_t address, uint16_t control, const char* message) {
+  uint16_t crc = 0;
+  crc ^= address; // XOR do valor atual do fcs com address
+  crc ^= control; // XOR do valor atual do fcs com control
 
   for (int i = 0; i < strlen(message); i++) {
-    fcs ^= message[i];
+    crc ^= message[i];
   }
-  return fcs;
+  return crc;
 }
